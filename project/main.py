5b85133cd749d8de73ebf2e5637976f9457e3374
Paul Ramberg <pramberg451@gmail.com>
2022-04-11 23:20:06 -0500

    Updated css, add datamuse hints, fixed bugs

--- project/main.py
+++ project/main.py
@@ -14,46 +14,69 @@ def get_new_word():
     word = random.choice(open("project/nouns.txt").readlines()).strip()
     return word
 
-def get_possible_hints(word):
+def get_trex_hint(word):
     base_url = "http://ngrams.ucd.ie/therex3/common-nouns/member.action?member={term}&kw={term}&needDisamb=false&xml=true"
     query = base_url.format(term=word)
     response = requests.get(query)
     json_string = json.dumps(xmltodict.parse(response.content))
     data = json.loads(json_string)
-
-    categories = []
-    for category in data['MemberData']['Categories']['Category']:
-        if int(category['@weight']) > 50:
-            categories.append(category)
     
-    return categories
-
-def get_new_hint():
-    if session['possible_hints']:
-        category = random.choice(session['possible_hints'])
+    categories = []
+    if len(data['MemberData']['Categories']) > 1:
+        for category in data['MemberData']['Categories']['Category']:
+            if int(category['@weight']) > 25:
+                categories.append(category)
+    
+    if categories:
+        category = random.choice(categories)
         descriptors = category['#text'].split(':')
-        hint = "".join(["The answer is a ", descriptors[0], " ", descriptors[1], "."])
+        hint = "".join(["I am a ", descriptors[0], " ", descriptors[1], "."])
     else:
         hint = 0
     return hint
 
-def get_comparative_hint(word):
+
+def get_comparative_trex_hint(word1, word2):
     base_url = "http://ngrams.ucd.ie/therex3/common-nouns/share.action?word1={word1}&word2={word2}&xml=true"
-    query = base_url.format(word1 = session['answer'], word2 = word)
+    query = base_url.format(word1 = word1, word2 = word2)
     response = requests.get(query)
     json_string = json.dumps(xmltodict.parse(response.content))
     data = json.loads(json_string)
-        
     categories = []
     if len(data['SharedCategory']['Members']) > 2:
-        for category in data['SharedCategory']['Members']['Member']:
-            #print(category)
-            if int(category['@weight']) > 50:
+        members = data['SharedCategory']['Members']['Member']
+        if not isinstance(members, list):
+            members = [members]
+        for category in members:
+            if int(category['@weight']) > 25:
                 categories.append(category)
-    if categories:    
+
+    if categories:
         category = random.choice(categories)
         descriptors = category['#text'].split(':')
-        hint = "".join(["The answer" + " and " + word + " are both ", descriptors[0], " ", descriptors[1], "s."])
+        hint = "".join(["The answer and " + word2 + " are both ", descriptors[0], " ", descriptors[1], "s."])
+    else:
+        hint = 0
+    return hint
+
+def get_dmuse_adjective(word):
+    base_url = "https://api.datamuse.com/words?rel_jjb={}&max=1"
+    query = base_url.format(word)
+    response = requests.get(query)
+    data = json.loads(response.content)
+    if data:
+        hint = "".join(["I could be described as " + data[0]['word'] + "."])
+    else:
+        hint = 0
+    return hint
+
+def get_dmuse_association(word):
+    base_url = "https://api.datamuse.com/words?rel_trg={}&max=1"
+    query = base_url.format(word)
+    response = requests.get(query)
+    data = json.loads(response.content)
+    if data:
+        hint = "".join(["I could be associated with " + data[0]['word'] + "."])
     else:
         hint = 0
     return hint
@@ -70,9 +93,11 @@ def get_similarity(answer, guess):
     word1 = Word.query.filter_by(word=answer).first()
     word1vector = frombuffer(word1.vector, float32)
     word2 = Word.query.filter_by(word=guess).first()
-    word2vector = frombuffer(word2.vector, float32)
-    
-    similarity = cosine_similarity(word1vector, word2vector)
+    if word2:
+        word2vector = frombuffer(word2.vector, float32)
+        similarity = round(cosine_similarity(word1vector, word2vector) * 100, 1)
+    else:
+        similarity = 0;
     return similarity
     
 
@@ -83,17 +108,17 @@ def index():
         session['new_riddle'] = True
         session['hints'] = []
         session['guesses'] = []
+        session['guess_count'] = 0
         session['riddle_completed'] = False
     
     # If a new riddle needs to be generated
     if session['new_riddle']:
-        # Get a new word to guess and its Thesaurus Rex data
+        # Get a new word to guess
         session['answer'] = get_new_word()
-        session['possible_hints'] = get_possible_hints(session['answer'])
         
         # Get an initial hint
         list = []
-        list.append(get_new_hint())
+        list.append(get_trex_hint(session['answer']))
         session['hints'] = list
         
         if current_user.is_authenticated:
@@ -101,17 +126,17 @@ def index():
 
         # Clear the guesses and reset completion status
         session['guesses'] = []
+        session['guess_count'] = 0
         session['riddle_completed'] = False
     
     return render_template('index.html')
 
-@main.route('/<int:is_guess>', methods=['POST'])
-def index_post(is_guess):
-    if is_guess:
+@main.route('/<int:forfeit>', methods=['POST'])
+def index_post(forfeit):
+    # If it's not a forfeit than it's a guess
+    if not forfeit:
         new_guess = request.form['guess'].lower()
-        if current_user.is_authenticated:
-            current_user.total_guesses += 1
-    
+        
         # If guess was correct
         if new_guess == session['answer']:
             flash('Answer found!')
@@ -127,30 +152,44 @@ def index_post(is_guess):
     
         # If guess was incorrect
         else:
+            session['guess_count'] += 1
+            
+            if current_user.is_authenticated:
+                current_user.total_hints += 1
+                current_user.total_guesses += 1
+            
             # Get similarity between the guess and the answer and add to the list
             similarity = get_similarity(session['answer'], new_guess)
-            list = session['guesses']
-            list.append([new_guess, similarity])
-            session['guesses'] = list
+            if not similarity:
+                flash('Not a valid word.')
+            else:
+                list = session['guesses']
+                list.append([new_guess, similarity])
+                session['guesses'] = list
 
-            # If another normal hint is possible (MAX 3)
-            if len(session.get('hints')) < 3:
-                hint = get_new_hint()
-                # If hint exists and its not a repeat add it to the hint list
-                if hint and hint not in session['hints']:
+            # If its the first guess
+            if session['guess_count'] == 1:
+                hint = get_dmuse_adjective(session['answer'])
+                # If hint exists add it to the hint list
+                if hint:
+                    list = session['hints']
+                    list.append(hint)
+                    session['hints'] = list
+
+            # If its the second guess
+            elif session['guess_count'] == 2:
+                hint = get_dmuse_association(session['answer'])
+                # If hint exists add it to the hint list
+                if hint:
                     list = session['hints']
                     list.append(hint)
                     session['hints'] = list
                 
-                if current_user.is_authenticated:
-                    current_user.total_hints += 1
-            
-            # If another comparative hint is possible (MAX 2)
-            elif len(session.get('hints')) < 5 or (len(session['possible_hints']) == len(session['hints'])):
+            elif session['guess_count'] > 2 or session['guess_count'] < 5:
                 # Get a comparative hint for each guess and if its vaild append it
                 comparative_hints = []
                 for guess in session['guesses']:
-                    comparative_hint = get_comparative_hint(guess[0])
+                    comparative_hint = get_comparative_trex_hint(session['answer'], guess[0])
                     if comparative_hint:
                         comparative_hints.append(comparative_hint)
                 
@@ -162,10 +201,7 @@ def index_post(is_guess):
                         list.append(hint)
                         session['hints'] = list
 
-                if current_user.is_authenticated:
-                    current_user.total_hints += 1
-    
-    # If it wasn't a guess then it was a forfeit
+    # It was a forfeit
     else:
         flash('The answer was ' + session['answer'] + ".")
         session['riddle_completed'] = True
